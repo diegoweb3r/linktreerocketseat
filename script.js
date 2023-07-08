cd8f306f002d0053791ce76697b8580fc39c31c7
Diego Weber <diegofelipeweber@hotmail.com>
2023-07-08 15:15:32 -0300

    complete link tree

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function switchMode() {
     let htmlFile = document.querySelector("html");
     if (htmlFile.classList.contains("light")){
         htmlFile.classList.remove("light");
+        image.src = "assets/avatar.png"
     } else {
         htmlFile.classList.add("light");
+        let image = document.getElementById("image");
+        image.src = "assets/avatar-light.png"
     }
 }
